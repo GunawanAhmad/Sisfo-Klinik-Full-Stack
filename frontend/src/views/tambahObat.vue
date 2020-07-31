066771bf9b5f5f0b1626ad05613aaaed1ec82127
Gunawan Nur Ahmad <54525132+Gunawanart@users.noreply.github.com>
2020-07-31 23:21:50 +0700

    add tambah obat fiture

---
+++ frontend/src/views/tambahObat.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="app">
+    <nav>
+      <div class="flexlogo">
+        <div class="back">
+          <img src="../../public/img/arrow.png" alt />
+          <a href="dashboard_pasien.html">
+            <p>kembali</p>
+          </a>
+        </div>
+        <div class="logo">
+          <a href="index.html">
+            <img src="../../public/img/kliniku.png" alt />
+          </a>
+        </div>
+      </div>
+      <h1>tambah obat</h1>
+    </nav>
+
+    <div class="container">
+      <div class="form">
+        <div class="nama section">
+          <label for="nama" class="label">Nama obat</label>
+          <input type="text" name="nama" class="input" v-model="namaObat" />
+        </div>
+
+        <form action class="satuan section">
+          <label for="satuan" class="label">Satuan</label>
+          <select name="satuan" id="satuan" class="input">
+            <option value="botol">botol</option>
+            <option value="pcs">pcs</option>
+            <option value="tube">tube</option>
+            <option value="stripe">stripe</option>
+            <option value="pack">pack</option>
+            <option value="tablet">tablet</option>
+          </select>
+        </form>
+        <div class="jumlah section">
+          <label for="jumlah" class="label">Jumlah</label>
+          <input type="number" name="jumlah" class="input" v-model="jumlah" />
+        </div>
+        <div class="tombol section">
+          <label class="label"></label>
+          <button class="input" @click="postObat">TAMBAH</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      namaObat: "",
+      jumlah: 0,
+    };
+  },
+  methods: {
+    postObat() {
+      let token = localStorage.getItem("token");
+      const satuan = document.querySelector("#satuan").value;
+      const body = {
+        namaObat: this.namaObat,
+        satuan: satuan,
+        jumlah: this.jumlah,
+      };
+      axios
+        .post("/staff/tambah-obat", body, {
+          headers: {
+            Authorization: "Barier " + token,
+          },
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped src="../../public/styles/tambah_obat.css">
+</style>
